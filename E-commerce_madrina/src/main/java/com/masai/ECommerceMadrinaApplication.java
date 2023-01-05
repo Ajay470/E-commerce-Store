94b042887060aa8535e0946dbdb6fd7be3f5443e
Ajay470 <105915458+Ajay470@users.noreply.github.com>
2023-01-05 20:39:27 +0530

    Setup Created

---
+++ E-commerce_madrina/src/main/java/com/masai/ECommerceMadrinaApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceMadrinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceMadrinaApplication.class, args);
+	}
+
+}
